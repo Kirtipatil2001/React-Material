39b52567c2bcd9b56c225060613a57738d4ec5d0
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2021-07-10 14:54:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import MaterialTable from 'material-table'
+
+
+function App() {
+  const [data, setData] = useState([])
+  const columns = [
+    { title: "Name", field: "name" },
+    { title: "Username", field: "username" },
+    { title: "Email", field: "email" },
+    { title: "Phone", field: "phone" },
+    { title: "Web Link", field: 'website' }
+  ]
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(resp => resp.json())
+      .then(resp => {
+        setData(resp)
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <h1 align="center">React-App</h1>
+      <h4 align='center'>Material Table with  CRUD operation</h4>
+      <MaterialTable
+        title="Employee Data"
+        data={data}
+        columns={columns}
+        editable={{
+          onRowAdd: (newRow) => new Promise((resolve, reject) => {
+            const updatedRows = [...data, { id: Math.floor(Math.random() * 100), ...newRow }]
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onRowDelete: selectedRow => new Promise((resolve, reject) => {
+            const index = selectedRow.tableData.id;
+            const updatedRows = [...data]
+            updatedRows.splice(index, 1)
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onRowUpdate:(updatedRow,oldRow)=>new Promise((resolve,reject)=>{
+            const index=oldRow.tableData.id;
+            const updatedRows=[...data]
+            updatedRows[index]=updatedRow
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          })
+
+        }}
+        options={{
+          actionsColumnIndex: -1, addRowPosition: "first"
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
